fa7ae784d6b4f60d996b330e2d86f02f43fe35cd
Sn1gga <46818215+Sn1gga@users.noreply.github.com>
2019-01-23 16:16:38 +0200

    add index and test

---
+++ index.js
@@ -0,0 +1,37 @@
+//Задание 2. Добавление в начало строки
+
+// var fs = require('fs');
+
+// fs.appendFile('test.txt', '\nHello, its new string', function(err){	
+// });
+
+
+//Задание 3. Отправка письма на почту
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'asnigur2019@gmail.com',
+    pass: 'snigurpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'asnigur2019@gmail.com',
+  to: 'lenasamotkan@gmail.com',
+  subject: 'Sending Email to myself using Node.js',
+  text: 'Easy peasy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+
+
